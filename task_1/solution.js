abb50286a3b5207c9e7049918eca78ed38f7e1bd
anastasia kalacheva <an.kalacheva@mail.ru>
2023-12-07 22:27:31 +0300

    task 1.3 fix

--- task_1/solution.js
+++ task_1/solution.js
@@ -40,10 +40,11 @@ function addInCartSumChange(sum, delta) {
     // Задание №1.3. Увеличение суммы в корзине
 
     let newSum = oldSum + difference;
-    let newSumText = newSum + ' ₽';
+    let newSumText = `${newSum} ₽`;
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
+    // newSumText = `1000 ₽`; newSumText = `${2 + 2} ₽`;
 
     // Конец решения задания №1.3.
 
