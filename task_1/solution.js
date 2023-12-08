4a7f0f4009e06af9dc6d8c01dd7f6e09c7e23d0d
anastasia kalacheva <an.kalacheva@mail.ru>
2023-12-08 19:57:41 +0300

    task 1.2 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -32,6 +32,11 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    
+    let newValue;
+    newValue = oldValue;
+    newValue = newValue+1;
+
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
